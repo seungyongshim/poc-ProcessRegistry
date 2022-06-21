cf51a4948ec17b9e92b798b5a2cb100c4bd23020
심승용 <seungyongshim@nexon.co.kr>
2022-06-21 14:35:14 +0900

    .

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Threading.Tasks;
+using Boost.Proto.Actor.DependencyInjection;
+using Boost.Proto.Actor.Hosting.Local;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Proto;
 using Xunit;
 using static Sample.Prelude;
 
@@ -7,8 +13,33 @@ namespace Sample.Tests;
 public class PreludeSpec
 {
     [Fact]
-    public void AddSuccess()
+    public async Task AddSuccessAsync()
     {
-        Assert.Equal(3, add(1)(2));
+        var host = Host.CreateDefaultBuilder()
+                       .UseProtoActor((sp, o) =>
+                       {
+                           o.FuncActorSystemStart = root =>
+                           {
+
+                               root.SpawnNamed(sp.GetRequiredService<IPropsFactory<HelloActor>>().Create(), "Hello");
+
+                               return root;
+                           };
+                       }).Build();
+
+        await host.StartAsync();
+
+        var root = host.Services.GetRequiredService<IRootContext>();
+
+        var ret = root.System.ProcessRegistry.Get(new("nonhost","Hello1"));
+
+        await host.StopAsync();
     }
+}
+
+
+public record HelloActor : IActor
+{
+    public Task ReceiveAsync(IContext context) =>
+        Task.CompletedTask;
 }
